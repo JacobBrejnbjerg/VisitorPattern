970aca5cbaf7b79317c8bfe5419af925e392d53d
William Winkler <williambwinkler@gmail.com>
2022-03-26 16:57:17 +0100

    Added IElement, som indeholder Accept-metoden. Employee arver fra IElement nu

--- VisitorPattern/Employee.cs
+++ VisitorPattern/Employee.cs
@@ -1,21 +1,23 @@
 ﻿namespace VisitorPattern
 {
-    public class Employee
+    public class Employee : IElement
     {
         public double Salary { get; set; }
         public int Seniority { get; set; }
         public int VacationDays { get; set; }
-
-        public void Accept(IVisitor visitor)
-        {
-            visitor.VisitEmployee(this);
-        }
-
+                
         public override string ToString()
         {
             return "Salary: " + Salary + "\r\n" +
                    "Seniority: " + Seniority + "\r\n" +
                    "VacationDays: " + VacationDays + "\r\n";
         }
+
+        // Implementerer Accept fra IElement
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitEmployee(this);
+        }
+
     }
 }
